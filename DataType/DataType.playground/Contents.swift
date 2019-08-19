e46829c9afa7c143d6a1963a60a16ec11d4995ed
Nelson <tomneo2004@gmail.com>
2019-08-19 20:38:55 +0800

    DataType Int

---
+++ DataType/DataType.playground/Contents.swift
@@ -0,0 +1,29 @@
+import Cocoa
+
+//////////////////////////////
+///        DataType        ///
+//////////////////////////////
+
+///Integer
+var integer : Int = 23
+integer = -9 //Integer accept both postive and negative number
+
+/**
+ Integer bounds min and max
+ */
+let min = Int.min
+let max = Int.max
+
+/**
+ Unsing integer
+ 
+ Unsign integer can only accept postive number
+ */
+var uInteger : UInt = 9
+//uInteger = -2 //Can not accept negative number
+
+/**
+ Unsign Integer bounds min and max
+ */
+let uMin = UInt8.min
+let uMax = UInt8.max
