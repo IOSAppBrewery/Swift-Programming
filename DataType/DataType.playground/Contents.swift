fe905409e74bce905ffd2f5067794dfa06674fce
Nelson <tomneo2004@gmail.com>
2019-08-20 17:10:30 +0800

    DataType update Float, Double, Boolean and Tuple

--- DataType/DataType.playground/Contents.swift
+++ DataType/DataType.playground/Contents.swift
@@ -27,3 +27,51 @@ var uInteger : UInt = 9
  */
 let uMin = UInt.min
 let uMax = UInt.max
+
+/**
+ Float
+ */
+var floatNumber : Float = 13.23
+floatNumber = -8.445
+
+
+/**
+ Double
+ */
+var doubleNumber : Double = 1143.2342837
+doubleNumber = -89765.283904
+
+/**
+ Boolean
+ */
+
+var boolean : Bool = true
+boolean = false
+
+/**
+ Tuples
+ */
+let (age, name, id, height) : (Int, String, String, Float) = (25, "Marry", "A8CF34", 170.25)
+print(age)
+print(id)
+print(name)
+print(height)
+
+//ignore middle name
+let (firstName, _, lastName) = ("John", "baker", "Doe")
+print(firstName)
+print(lastName)
+
+//give name to individual elements with tuple
+let employee = (firName:"Jane", lastName:"Park", age:30, department:"HR")
+print(employee.firName)
+print(employee.lastName)
+print(employee.age)
+print(employee.department)
+
+//retrieve value from tuple by index
+let employee2 = ("Mac", lastName:"White", age:28, department:"IT")
+print(employee2.0)
+print(employee2.1)
+print(employee2.2)
+print(employee2.3)
